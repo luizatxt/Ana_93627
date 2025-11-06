4962c239bc1ba65b724b561a9ed2576b4821d57e
Aluno Feira <aluno.fsa@senaibahia.com.br>
2025-11-06 10:45:42 -0300

    flask

---
+++ app.py
@@ -0,0 +1,17 @@
+# INCLUIDO A BIBLIOTECA
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+# Página principal
+@app.route('/')
+def home():return render_template('index.html')
+
+@app.route('/projeto')
+def projeto():return render_template('projeto.html')
+
+# API simples
+@app.route('/api/mensagem')
+def mensagem():return jsonify({'mensagem':'Olá, mundo!'})
+# Iniciar o site
+
+if __name__ == '__main__':app.run(debug=True)
